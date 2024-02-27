d4104d0c2d05038edf15baf28b3bbc3e3d6fe659
Abdullah Albayati <dark.king1154@gmail.com>
2024-02-26 20:46:23 -0600

    Started with Loops

---
+++ js-loops/01-loops-intro.js
@@ -0,0 +1,32 @@
+// print 1,2,3,4,5,6,7,8,9,10
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// backwords 5,4,3,....
+
+for (let i = 5; i >= 1; i--) {
+  console.log(i);
+}
+
+// 0, 2, 4, 6 ....
+
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+// 50, 40, 30, 20 ...
+
+for (let i = 50; i >= 0; i -= 10) {
+  console.log(i);
+}
+
+// print and save it in array 1,2,3,4,5,6,7,8,9,10
+let arr = [];
+
+for (let i = 1; i <= 10; i++) {
+  arr.push(i);
+}
+
+console.log(arr.join(" ,"));
