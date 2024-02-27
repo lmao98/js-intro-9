424095d151b0728143082907e86aee1e0b561193
Abdullah Albayati <dark.king1154@gmail.com>
2024-02-26 20:22:09 -0600

    Added array destructring

---
+++ js-arrays/13-destrucuring-array.js
@@ -0,0 +1,15 @@
+const favMoveis = ["Harry Potter", "GodFather", "Lord of the Rings"];
+
+// Destructring
+const [fav01, fav02, fav03] = favMoveis;
+
+console.log(fav01);
+
+const credentials = ["TechGlobalSchool", "Test123", "techglobal@gmail.com"];
+// username, password, email
+
+const [username, password, email] = credentials;
+
+console.log(username);
+console.log(password);
+console.log(email);
